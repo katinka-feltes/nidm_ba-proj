c124187f08584d5ce02f54f81cedee0622a4f08c
katinka-feltes <48929802+katinka-feltes@users.noreply.github.com>
2023-01-11 16:35:34 +0100

    added controller and first output of one model

---
+++ NIDM/app/resources/simulations/SimulationsController.jl
@@ -0,0 +1,19 @@
+module SimulationsController
+
+  using Agents
+
+  import Main.Model as Model
+  model = Model.initialize_model(number_of_agents = 24, α = 0.75, c2 = 0.1, Φ = 10)
+
+  using Genie.Renderer.Html
+  function agents()
+    html(:simulations, :agents, agents = allagents(model))
+  end
+
+  function newmodel()
+    model = Model.initialize_model(number_of_agents = 5, α = 0.75, c2 = 0.1, Φ = 10)
+
+    html(:simulations, :agents, agents = allagents(model))
+  end
+  
+end
--- NIDM/routes.jl
+++ NIDM/routes.jl
@@ -3,3 +3,7 @@ using Genie.Router
 route("/") do
   serve_static_file("NIDM.html")
 end
+
+route("/agents", SimulationsController.agents)
+
+route("/new", SimulationsController.newmodel)
