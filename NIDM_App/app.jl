3d41901b9cbd84d090b821d002779dd3c1dac1fe
Helen Kuswik <hkuswik@web.de>
2023-01-14 13:18:20 +0100

    tried more stuff with genie builder, connecting model and interface worked

--- NIDM_App/app.jl
+++ NIDM_App/app.jl
@@ -1,4 +1,5 @@
 using GenieFramework
+using Genie.Renderer.Html
 
 include("model.jl")
 using .Model
@@ -7,10 +8,23 @@ using Agents
 @genietools
 
 model = Model.initialize_model(number_of_agents = 24, α = 0.75, c2 = 0.1, Φ = 10)
-const agents = allagents(model)
+@out const min = 0
+@out const max = 50
 
 @handlers begin
-  
+    @in agents_amount = 20 #default values so UI can be renderen when page loads
+    @in alpha = 0.75
+    @in auswahl = ["A", "B", "C"]
+    @out value_agents = agents_amount
+    @out value_alpha = alpha
+    @out agents = allagents(model)
+    @out list = html("<li> bla </li>") # anscheinend nicht so
+    @onchangeany agents_amount, alpha begin
+        value_agents = agents_amount
+        value_alpha = alpha
+        model2 = Model.initialize_model(number_of_agents = agents_amount, α = alpha, c2 = 0.1)
+        agents = allagents(model2)
+    end
 end
 
 @page("/", "app.jl.html")
