65cc347992f867b39a7684e38b94fe981ef0c3ec
katinka-feltes <48929802+katinka-feltes@users.noreply.github.com>
2023-01-19 15:42:25 +0100

    added transition between two graphs

--- NIDM/public/code_graph.js
+++ NIDM/public/code_graph.js
@@ -1,7 +1,7 @@
 var svg = d3.select("svg"),
 width = +svg.node().getBoundingClientRect().width,
 height = +svg.node().getBoundingClientRect().height;
-var graph;
+var graph, data1, data2;
 
 // svg objects
 var link, node;
@@ -11,21 +11,19 @@ var simulation = d3.forceSimulation()
 // load the data
 d3.json("model.json", function(error, _graph) {
 if (error) throw error;
-graph = _graph;
-
+data1 = _graph;
+graph = data1;
 initializeDisplay();
 initializeSimulation();
 });
 
-
+// preload the extra data
+d3.json("model_new.json", function(error, _graph) {
+    if (error) throw error;
+    data2 = _graph;
+});
 
 function initializeDisplay() {
-
-    function color(status){
-        if (status == "S") return "#A4D3EE";
-        else if (status == "I") return "#EE2c2c";
-        else return "#8fbc8f" //R
-    }
 
     //add the links
     link = svg.append("g")
@@ -78,4 +76,52 @@ function ticked() {
         .attr("cy", function(d) { return d.y; });
 }
 
+// Create a function that takes a dataset as input and update the plot:
+function update(data) {
+
+    graph = data;
+
+    simulation.nodes(graph.nodes);
+    simulation.force("link", d3.forceLink()                       // This force provides links between nodes
+            .id(function(d) { return d.id; })                     // This provide  the id of a node
+            .links(graph.links)                                   // and this the list of links
+        )
+
+    // Create the u variable
+    var u = svg.selectAll("line ")
+        .data(graph.links)
+
+    // If less group in the new dataset, I delete the ones not in use anymore
+    u.exit()
+        .transition() // and apply changes to all of them
+        .duration(1000)
+        .style("opacity", 0)
+        .remove()   
     
+    u.enter()
+        .append("line") // Add a new line for each new elements
+        .merge(u) // get the already existing elements as well
+        .transition()
+        .duration(2000)
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    svg.selectAll("circle")
+        .data(graph.nodes)
+        .transition()
+        .duration(2000)
+        .attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; })
+        .style("fill", function(d){ return color(d.health_status)});
+
+}    
+    
+
+
+function color(status){
+    if (status == "S") return "#A4D3EE";
+    else if (status == "I") return "#EE2c2c";
+    else return "#8fbc8f" //R
+}
