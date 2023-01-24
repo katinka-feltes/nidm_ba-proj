b359cc62600ca243a3495798fee728475323d51f
Helen Kuswik <hkuswik@web.de>
2023-01-24 13:22:36 +0100

    finished sliders functionality; added reset button + functionality to return to default values

--- NIDM/public/js/input_functionality.js
+++ NIDM/public/js/input_functionality.js
@@ -1,19 +1,26 @@
-var rangeSlider = function(){
-    var slider = $('.slider'),
-        range = $('.range-slider__range'),
-        value = $('.range-slider__value');
-        
-    slider.each(function(){
+function updateTextInput(val, id) {
+    document.getElementById(id).textContent = val; 
+}
 
-        value.each(function(){
-        var value = $(this).prev().attr('value');
-        $(this).html(value);
-        });
+function updateMaxValue(val) {
+    document.getElementById('phi-slider').max = val-1;
+}
 
-        range.on('input', function(){
-        $(this).next(value).html(this.value);
-        });
-    });
-};
-  
-rangeSlider();
+function reset() {
+    document.getElementById('agent-slider').value = 20;
+    document.getElementById('agents-amount').textContent = 20;
+    document.getElementById('risk-slider').value = 0.5;
+    document.getElementById('risk').textContent = 0.5;
+    document.getElementById('sigma-slider').value = 5;
+    document.getElementById('sigma').textContent = 5;
+    document.getElementById('tau-slider').value = 10;
+    document.getElementById('tau').textContent = 10;
+    document.getElementById('phi-slider').value = 10;
+    document.getElementById('phi').textContent = 10;
+    document.getElementById('alpha-slider').value = 0.75;
+    document.getElementById('alpha').textContent = 0.75;
+    document.getElementById('c2-slider').value = 0.1;
+    document.getElementById('c2').textContent = 0.1;
+    document.getElementById('gamma-slider').value = 0.5;
+    document.getElementById('gamma').textContent = 0.5;
+}
