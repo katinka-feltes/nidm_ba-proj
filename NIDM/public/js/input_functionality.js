501ddc27cb948bd4bbded66f01de9c85f6b66acb
katinka-feltes <48929802+katinka-feltes@users.noreply.github.com>
2023-01-25 14:13:44 +0100

    changed get button

--- NIDM/public/js/input_functionality.js
+++ NIDM/public/js/input_functionality.js
@@ -20,17 +20,17 @@ function reset() {
     document.getElementById('agent-slider').value = 20;
     document.getElementById('agents-amount').textContent = 20;
     document.getElementById('risk-slider').value = 0.5;
-    document.getElementById('risk').textContent = 0.5;
+    //document.getElementById('risk').textContent = 0.5;
     document.getElementById('sigma-slider').value = 5;
-    document.getElementById('sigma').textContent = 5;
+    //document.getElementById('sigma').textContent = 5;
     document.getElementById('tau-slider').value = 10;
-    document.getElementById('tau').textContent = 10;
+    //document.getElementById('tau').textContent = 10;
     document.getElementById('phi-slider').value = 10;
     document.getElementById('phi').textContent = 10;
     document.getElementById('alpha-slider').value = 0.75;
-    document.getElementById('alpha').textContent = 0.75;
+    //document.getElementById('alpha').textContent = 0.75;
     document.getElementById('c2-slider').value = 0.1;
-    document.getElementById('c2').textContent = 0.1;
+    //document.getElementById('c2').textContent = 0.1;
     document.getElementById('gamma-slider').value = 0.5;
     document.getElementById('gamma').textContent = '50%';
 }
